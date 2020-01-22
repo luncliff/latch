170c7dcc06f3359208c8a9b887e86085eab6dc6c
Park DongHa <luncliff@gmail.com>
2020-01-22 23:23:34 +0900

    vcpkg: install catch2
    * cmake: use toolchain vcpkg by default

--- scripts/build-vs2017-cmake-clang.bat
+++ scripts/build-vs2017-cmake-clang.bat
@@ -31,6 +31,6 @@ REM We can specify with -DCMAKE_CXX_COMPILER=clang-cl
 REM But for this script we will use CXX variable
 set CXX=clang-cl
 
-cmake . -G "Ninja" -D CMAKE_INSTALL_PREFIX="./install" -D BUILD_SHARED_LIBS="%SHARED%" -D CMAKE_BUILD_TYPE="%BUILD_TYPE%"
+cmake . -G "Ninja" -DCMAKE_INSTALL_PREFIX="./install" -DBUILD_SHARED_LIBS="%SHARED%" -DCMAKE_BUILD_TYPE="%BUILD_TYPE%" -DCMAKE_TOOLCHAIN_FILE="C:\vcpkg\scripts\buildsystems\vcpkg.cmake"
 cmake --build .
 cmake --build . --target install
