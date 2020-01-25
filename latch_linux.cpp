0510d31a1dad035a13bc209eb8e865d01670de6f
luncliff <luncliff@gmail.com>
2020-01-26 00:55:55 +0900

    linux: implementing operations with syscall

--- latch_linux.cpp
+++ latch_linux.cpp
@@ -3,9 +3,30 @@
 // clang-format off
 #include <system_error>
 
+#include <unistd.h>
+#include <sys/syscall.h>
 #include <linux/futex.h>
 #include <sys/time.h>
 // clang-format on
+
+/**
+ * @link http://manpages.ubuntu.com/manpages/bionic/man2/futex.2.html
+ */
+int futex_wait_on_address(ptrdiff_t* target, ptrdiff_t value, const struct timespec *timeout)
+{
+    // EAGAIN?
+    return syscall(SYS_futex, target, FUTEX_WAIT_PRIVATE, value,
+                   timeout, NULL, 0);
+}
+
+/**
+ * @link http://manpages.ubuntu.com/manpages/bionic/man2/futex.2.html
+ */
+int futex_wake_by_address(ptrdiff_t* target)
+{
+    return syscall(SYS_futex, target, FUTEX_WAKE, 1,
+                       NULL, NULL, 0);
+}
 
 namespace std {
 
@@ -23,8 +44,12 @@ namespace std {
         static_assert(is_same_v<ptrdiff_t, int64_t> || is_same_v<ptrdiff_t, int32_t>);
         if (counter < update)
             throw system_error{EINVAL, system_category(), "update is greater than counter"};
-
+        /// @todo perform atomic operation
+        counter -= update;
         /// @todo need implementation of wake
+        if(counter == 0)
+            if(futex_wake_by_address(&counter)!=0)
+                throw system_error{errno, system_category(), "futex_wake_by_address"};
     }
 
     bool latch::try_wait() const noexcept {
@@ -32,6 +57,8 @@ namespace std {
             return true;
 
         /// @todo need implementation of wait
+        if(futex_wait_on_address(const_cast<ptrdiff_t*>(&counter), 0, nullptr))
+            return counter == 0;
         return false;
     }
 
@@ -39,7 +66,7 @@ namespace std {
         while (try_wait() == false) {
             /// @todo need implementation for spurious wakeup
             if (errno)
-                throw system_error{static_cast<int>(errno), system_category(), "WaitOnAddress"};
+                throw system_error{errno, system_category(), "futex_wait_on_address"};
         }
     }
 
